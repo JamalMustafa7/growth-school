c813eb11126df6649a58911b6ad94560ef21bbb2
JamalMustafa17 <muhammedjamalmustafa@gmail.com>
2024-07-25 15:59:42 +0500

    make navbar responsive and finalize it

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 const dropdownButton=document.querySelector(".dropdown-button");
 const dropdown=document.querySelector(".dropdown");
+const hamburger=document.querySelector(".hamburger");
+const innerLink=document.querySelector(".nav-inner-links");
+
+document.addEventListener('DOMContentLoaded',function()
+{
+    if(window.innerWidth<=850)
+    {
+        innerLink.classList.add("hide");
+    }
+})
+window.addEventListener('resize', function() {
+    if(window.innerWidth<=850)
+        {
+            innerLink.classList.add("hide");
+        }
+    else
+        innerLink.classList.remove("hide");
+});
 dropdownButton.addEventListener("click",function(event)
 {
+    innerLink.classList.add("hide");
+
     dropdown.classList.toggle("hide");
     event.stopPropagation();
 })
@@ -9,4 +29,12 @@ dropdownButton.addEventListener("click",function(event)
 document.addEventListener("click",function(event)
 {
     dropdown.classList.add("hide")
+    innerLink.classList.add("hide");
+})
+
+hamburger.addEventListener("click",function(event)
+{
+    dropdown.classList.add("hide")
+    innerLink.classList.toggle("hide")
+    event.stopPropagation();
 })
